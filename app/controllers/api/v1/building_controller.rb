fb778669f0b7810b96f3d7625e5df2f624b8f580
Zach Dubno <zachedubno@gmail.com>
2017-08-16 11:42:34 -0400

    cleaned up data with script and removed outliers from average query

--- app/controllers/api/v1/building_controller.rb
+++ app/controllers/api/v1/building_controller.rb
@@ -12,12 +12,22 @@ class Api::V1::BuildingController < ApplicationController
     end
 
     if building
-      buildings_for_average = Building.all.where("property_floor_area < '#{building.property_floor_area + 25_000}' and property_floor_area > '#{building.property_floor_area - 25_000}'")
+      buildings_for_average = Building.all.where(
+        "property_floor_area < '#{building.property_floor_area + 25_000}'
+        and property_floor_area > '#{building.property_floor_area - 25_000}'
+        and site_eui < '1000'"
+      )
+      # .select do |b|
+      #   b[:site_EUI] < 1000
+      # end
+      puts buildings_for_average.average(:site_eui).to_i
       render json: {
         building: building,
         averages: {
-          indirectGHG: buildings_for_average.average(:indirect_GHG_emissions).to_i,
-          directGHG: buildings_for_average.average(:direct_GHG_emissions).to_i
+          indirect_ghg: buildings_for_average.average(:indirect_ghg_emissions).to_i,
+          direct_ghg: buildings_for_average.average(:direct_ghg_emissions).to_i,
+          site_eui: buildings_for_average.average(:site_eui).to_i,
+          source_eui: buildings_for_average.average(:source_eui).to_i
         }
       }
     else
--- db/migrate/20170808153621_create_building.rb
+++ db/migrate/20170808153621_create_building.rb
@@ -1,24 +1,24 @@
 class CreateBuilding < ActiveRecord::Migration[5.1]
   def change
     create_table :buildings do |t|
-      t.bigint :BBL
-      t.string :BIN
+      t.bigint :bbl
+      t.string :bin
       t.string :street_number
       t.string :street_name
       t.string :borough
       t.integer :zipcode
       t.string :on_covered_building_list
-      t.string :DOF_Benchmarking_status
-      t.float :site_EUI
-      t.float :weather_normalized_site_EUI
-      t.float :source_EUI
-      t.float :weather_normalized_source_EUI
+      t.string :dof_benchmarking_status
+      t.float :site_eui
+      t.float :weather_normalized_site_eui
+      t.float :source_eui
+      t.float :weather_normalized_source_eui
       t.float :indoor_water_intensity
       t.string :reported_water_method
       t.integer :energy_star_score
-      t.float :total_GHG_emissions
-      t.float :direct_GHG_emissions
-      t.float :indirect_GHG_emissions
+      t.float :total_ghg_emissions
+      t.float :direct_ghg_emissions
+      t.float :indirect_ghg_emissions
       t.integer :property_floor_area
       t.string :primary_property_type
     end
